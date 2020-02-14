5a65a7154fdde3c038ca6bc70547fbb78835461b
arekotto <23309349+arekotto@users.noreply.github.com>
2020-02-14 14:16:10 +0100

    init

---
+++ VirtualBusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VirtualBusinessCard
+//
+//  Created by Arek Otto on 14/02/2020.
+//  Copyright © 2020 Arek Otto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
